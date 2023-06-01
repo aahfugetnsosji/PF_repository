97f35e09c24cb7ecdc35ddffcd9438686ce0b517
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-01 16:04:13 +0000

    [Add] ユーザ/新規登録・ログイン・ログアウト 管理者/ログイン・ログアウト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  
+  # ユーザー用devise
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  # 管理者用devise
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # ユーザ用
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about', as: 'about'
+  end
+  
+  # 管理者用
+  namespace :admin do
+    
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
