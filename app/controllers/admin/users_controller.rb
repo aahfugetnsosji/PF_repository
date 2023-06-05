f73aa66535a2582b2704f5d9278ef86bd69c86e9
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-05 13:04:16 +0000

    [Update] 管理者：ユーザ退会後の遷移先を変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,10 +10,12 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     if @user.email == "guest@example.com"
-      redirect_to admin_users_path, alert: "ゲストユーザーは削除できません。"
-    else
+      redirect_to admin_user_path(@user.id), alert: "ゲストユーザーは削除できません。"
+    elsif @user.email != "guest@example.com"
       @user.destroy
-      redirect_to admin_user_path(@user.id), notice: "退会処理が完了しました。"
+      redirect_to admin_users_path, notice: "退会処理が完了しました。"
+    else
+      render :show, alert: "退会処理が完了できませんでした。"
     end
   end
 end
