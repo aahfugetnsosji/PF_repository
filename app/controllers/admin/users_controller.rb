8f10999d9d814873b13859a9f93c5194a8b5560c
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-05 11:36:02 +0000

    [Add] 管理者:ユーザ一覧・ユーザ詳細・ユーザ退会

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,19 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to admin_users_path, alert: "ゲストユーザーは削除できません。"
+    else
+      @user.destroy
+      redirect_to admin_user_path(@user.id), notice: "退会処理が完了しました。"
+    end
   end
 end
