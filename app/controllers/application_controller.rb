4ea468ec2e151e557c774154b5a22603b39a7efd
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-07 16:06:01 +0000

    [Add] 管理者：タグの追加・一覧・編集・削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,44 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? #新規登録時にnameの値を受け取る
   before_action :ensure_normal_user, only: :destroy, if: -> { controller_name == "registrations" } #ゲストユーザの削除制限用
+  before_action :authenticate_admin!, if: :admin_url # 管理者用ページへのアクセス制限
+
+  private
+    # 管理者用ページの定義
+    def admin_url
+      request.fullpath.include?("/admin")
+    end
+    
+    # ログイン後の遷移先
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        root_path #あとで通報一覧ページに変更
+      else
+        root_path
+      end
+    end
+    
+    # ログアウト後の遷移先
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :admin
+        new_admin_session_path
+      elsif resource_or_scope == :public
+        root_path
+      else
+        root_path
+      end
+    end
 
   protected
-
-# 新規登録時にnameの値を受け取る
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-# ゲストユーザの削除制限
-  def ensure_normal_user
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    # 新規登録時にnameの値を受け取る
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
-  end
+
+    # ゲストユーザの削除制限
+    def ensure_normal_user
+      if resource.email == 'guest@example.com'
+        redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :show, :destroy]
     resources :posts, only: [:index, :show, :destroy]
+    resources :tags, except: [:show, :new]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
