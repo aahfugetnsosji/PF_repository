22a40da6b6d5bb16789e191b19210ab5c582be9e
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-11 19:07:47 +0000

    [Finish] 管理者：投稿一覧・投稿詳細・投稿削除

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -5,6 +5,16 @@ class Admin::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @post_comments = @post.post_comments.all
   end
   
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      redirect_to admin_posts_path
+      flash[notice] = "投稿を削除しました。"
+    else
+      render :show, alert: "投稿の削除に失敗しました。"
+    end
+  end
 end
--- app/controllers/admin/regions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::RegionsController < ApplicationController
-  
-  def destroy
-    @region = Region.find(params[:id])
-    @region.destroy
-    redirect_to admin_posts_path
-  end
-  
-end
