41f67fec3477ffe5f3856bf28e1200b529fb56da
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-11 19:09:49 +0000

    [Add] 管理者：コメント一覧・削除

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,2 +1,7 @@
 class Admin::PostCommentsController < ApplicationController
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to admin_post_path(params[:post_id])
+    flash[:notice] = "コメントを削除しました。"
+  end
 end
