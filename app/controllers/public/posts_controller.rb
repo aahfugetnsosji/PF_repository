142ac2f4229005576f04f5a4e74329af55699795
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-17 06:44:44 +0000

    [Add] ユーザ：タグ検索(未完了)

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
   end
 
   def new
