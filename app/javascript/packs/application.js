464204b37bb4cd1e6eb31c7c663a829b271991da
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-06-24 19:40:45 +0000

    [Update] Jmap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
-import '../stylesheets/mystyle.css'
+import '../stylesheets/mystyle.css';
 
 
 Rails.start()
