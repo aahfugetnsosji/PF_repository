deb737c383dd35c0a57c6add2017162737e60eae
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-07-02 18:04:29 +0000

    [Update] ユーザ：Topページに検索窓を設置

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,12 @@
 class Public::HomesController < ApplicationController
   def top
+    # 最新の投稿12件を取得
+    @latest_posts = Post.order(created_at: :desc).limit(12)
+    @tags = Tag.all
+    @regions = Region.all
+    @prefectures = Prefecture.all
   end
-  
+
   def about
   end
 end
