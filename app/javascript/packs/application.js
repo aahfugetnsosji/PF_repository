a07b82a1275a1160b8c4692279fc8261332fd703
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-07-06 14:00:23 +0000

    [Update] レイアウト調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
+import "./smoothscroll-min"; // スクロールボタンをIEとSafariに対応させるためのファイル
 import "./jmap"; // japanMapの読み込み
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+import "./mystyle"; // 独自のjsファイル読み込み
 
 
 Rails.start()
