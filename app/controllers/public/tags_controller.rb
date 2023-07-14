6c38c6240436ac03de61f837803895369615fa14
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-07-14 18:51:24 +0000

    [Add] ユーザ：タグ検索結果専用ページを作成

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -1,2 +1,8 @@
 class Public::TagsController < ApplicationController
+  def index
+    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
+    @tags = Tag.all
+    @regions = Region.all
+    @prefectures = Prefecture.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,6 @@ Rails.application.routes.draw do
     get 'mypage/edit' => "users#edit"
     get 'unsubscribe' => "users#unsubscribe"
     get 'mypage/favorites' => "users#favorite"
-    # 地方区分絞り込み用
-    resources :regions, only: [:index]
     # 投稿
     resources :posts do
       # コメント用
@@ -40,8 +38,12 @@ Rails.application.routes.draw do
     end
     # キーワード検索用
     get 'search' => "posts#search"
+    # 地方区分絞り込み用
+    resources :regions, only: [:index]
     # 都道府県絞り込み用
     resources :prefectures, only: [:index]
+    # タグ検索用
+    resources :tags, only: [:index]
     # 通報用
     resources :reports, only: [:new, :create]
   end
