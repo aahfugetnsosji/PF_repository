46b130705a5b8377a5112022f15e9eb01ae2fe1b
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-07-19 20:10:25 +0000

    [Update] レイアウト調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,8 @@ class User < ApplicationRecord
   # プロフィール画像
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
-      profile_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
+      file_path = Rails.root.join('app/assets/images/user-icon.png')
+      profile_image.attach(io: File.open(file_path), filename: 'user-icon.png', content_type: 'image/png')
     end
     profile_image.variant(resize_to_fill: [width, height]).processed
   end
