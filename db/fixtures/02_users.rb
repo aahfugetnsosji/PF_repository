a2f2704cf7b952544babaea49706f02a9ecf5ebb
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-10-26 19:05:54 +0000

    [Update] 初期データ追加

--- db/fixtures/02_users.rb
+++ db/fixtures/02_users.rb
@@ -27,17 +27,26 @@ User.seed(:email,
   },
   {
     id: 4,
-    name: 'サヨ子',
-    email: 'sayoko@example.com',
-    password: 'password',
-    profile: '京都行きたい。'
-  },
-  {
-    id: 5,
     name: '虚友達',
     email: 'imaginary-friend@example.com',
     password: 'password',
-    profile: 'みんなほんとに見えないの？'
+    profile: 'みんなほんとに見えないの？',
+    profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")
+  },
+  {
+    id: 5,
+    name: '○○市観光協会広報班',
+    email: 'oidemase@example.com',
+    password: 'password',
+    profile: '○○市の観光情報を発信しています。',
+    profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")
+  },
+  {
+    id: 6,
+    name: '山田哲子',
+    email: 'yamada-tetsuko@example.com',
+    password: 'aaa123',
+    profile: '某選手とは一切関係ありません。'
   }
 )
 
